10e634efb8ed6be0bb3d8a977cea36e649a4f955
Graham Walker <contact@gwalkerux.com>
2020-12-02 02:26:53 -0500

    Moved error repairing to its own class

---
+++ youtube-dl-express-backend/utilities/error.utility.js
@@ -0,0 +1,56 @@
+import { spawnSync } from 'child_process';
+import path from 'path';
+
+import DownloadError from '../models/error.model.js';
+
+export default class ErrorManager {
+    constructor() {
+        this.busy = false;
+    }
+
+    async repair(errorId) {
+        this.busy = true;
+        let error;
+        try {
+            error = await DownloadError.findOne({ _id: errorId });
+        } catch (err) {
+            this.busy = false;
+            return { error: 'Could not find error document' };
+        }
+
+        const execProcess = await spawnSync(`npm${process.platform === 'win32' ? '.cmd' : ''}`, [
+            'run',
+            'exec',
+            '--',
+            '--job-id',
+            error.jobDocument,
+            '--is-repair',
+            '--error-id',
+            error._id,
+            '--video',
+            path.join(parsedEnv.OUTPUT_DIRECTORY, 'videos', error.videoPath),
+        ], { windowsHide: true });
+
+        if (execProcess.status !== 0) {
+            this.busy = false;
+            throw {
+                error: `Exec failed to repair the download with status`
+                    + ` code: ${execProcess.status}. Refresh the page to see the updated error.`
+            };
+        }
+
+        try {
+            await DownloadError.findByIdAndDelete(error._id);
+        } catch (err) {
+            this.busy = false;
+            throw err;
+        }
+
+        this.busy = false;
+        return { success: 'Video repaired' };
+    }
+
+    isBusy() {
+        return this.busy;
+    }
+}
