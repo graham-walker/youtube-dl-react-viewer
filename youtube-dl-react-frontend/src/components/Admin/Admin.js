c7a00fbc3dde51a425c7236fa0198c4d2e89114f
Graham Walker <contact@gwalkerux.com>
2021-08-25 13:59:43 -0400

    Updated default arguments used by jobs

--- youtube-dl-react-frontend/src/components/Admin/Admin.js
+++ youtube-dl-react-frontend/src/components/Admin/Admin.js
@@ -582,7 +582,7 @@ const defaultArguments = `# Some options are not included here are are set when 
 # --batch-file
 # --keep-fragments
 
-# These options are set by the script when executing youtube-dl. Do not include them here, they will be overridden:
+# These options are set by the script when a job is run. Do not set them here, they will be overridden:
 # --exec
 # --write-info-json
 # --prefer-ffmpeg
@@ -593,28 +593,38 @@ const defaultArguments = `# Some options are not included here are are set when 
 # --download-archive
 # --cache-dir
 
-# These options may help the script run but are not necessary. They can be freely modified or removed:
---geo-bypass
---force-ipv4
+# These options are set by the script if using the yt-dlp fork of youtube-dl. Do not include them here, they will be overridden:
+# --compat-options youtube-dl
 
-# If you are encountering 429 errors on YouTube you might want to consider including these options, or signing in:
-#--sleep-interval 5
-#--max-sleep-interval 30
-#--cookies "/Path/To/cookies.txt"
-
-# These options are not required, but are preferred by the script:
+# These options are not required, but are preferred and are used by default:
 # --write-all-thumbnails can be replaced with --write-thumbnail and --all-subs can be replaced with --write-sub or --write-auto-sub
+# Changing --merge-output-format mkv might cause some merges to fail depending on the format code used
 --write-description
 --write-annotations
 --write-all-thumbnails
 --all-subs
---sub-format srt
 --merge-output-format mkv
 --add-metadata
 --xattrs
 --xattr-set-filesize
 --no-overwrites
 --no-continue
---ignore-errors`;
+--ignore-errors
+
+# These options may help the script but are not necessary. They can be freely modified:
+#--geo-bypass
+#--force-ipv4
+#--match-filter "!is_live & !live"
+
+# To prevent 429 errors on YouTube you might want to consider including these options, or sign in using cookies:
+#--sleep-interval 5
+#--max-sleep-interval 30
+#--cookies "/Path/To/cookies.txt"
+
+# These options are recommended, but are only available if using the yt-dlp fork of youtube-dl:
+#--sleep-requests 1
+#--write-comments
+#--datebefore "$(date --date="30 days ago" +%Y%m%d)"
+`;
 const defaultFormatCode = '(bestvideo[vcodec^=av01][height>=4320][fps>30]/bestvideo[vcodec^=vp9.2][height>=4320][fps>30]/bestvideo[vcodec^=vp9][height>=4320][fps>30]/bestvideo[vcodec^=avc1][height>=4320][fps>30]/bestvideo[height>=4320][fps>30]/bestvideo[vcodec^=av01][height>=4320]/bestvideo[vcodec^=vp9.2][height>=4320]/bestvideo[vcodec^=vp9][height>=4320]/bestvideo[vcodec^=avc1][height>=4320]/bestvideo[height>=4320]/bestvideo[vcodec^=av01][height>=2880][fps>30]/bestvideo[vcodec^=vp9.2][height>=2880][fps>30]/bestvideo[vcodec^=vp9][height>=2880][fps>30]/bestvideo[vcodec^=avc1][height>=2880][fps>30]/bestvideo[height>=2880][fps>30]/bestvideo[vcodec^=av01][height>=2880]/bestvideo[vcodec^=vp9.2][height>=2880]/bestvideo[vcodec^=vp9][height>=2880]/bestvideo[vcodec^=avc1][height>=2880]/bestvideo[height>=2880]/bestvideo[vcodec^=av01][height>=2160][fps>30]/bestvideo[vcodec^=vp9.2][height>=2160][fps>30]/bestvideo[vcodec^=vp9][height>=2160][fps>30]/bestvideo[vcodec^=avc1][height>=2160][fps>30]/bestvideo[height>=2160][fps>30]/bestvideo[vcodec^=av01][height>=2160]/bestvideo[vcodec^=vp9.2][height>=2160]/bestvideo[vcodec^=vp9][height>=2160]/bestvideo[vcodec^=avc1][height>=2160]/bestvideo[height>=2160]/bestvideo[vcodec^=av01][height>=1440][fps>30]/bestvideo[vcodec^=vp9.2][height>=1440][fps>30]/bestvideo[vcodec^=vp9][height>=1440][fps>30]/bestvideo[vcodec^=avc1][height>=1440][fps>30]/bestvideo[height>=1440][fps>30]/bestvideo[vcodec^=av01][height>=1440]/bestvideo[vcodec^=vp9.2][height>=1440]/bestvideo[vcodec^=vp9][height>=1440]/bestvideo[vcodec^=avc1][height>=1440]/bestvideo[height>=1440]/bestvideo[vcodec^=av01][height>=1080][fps>30]/bestvideo[vcodec^=vp9.2][height>=1080][fps>30]/bestvideo[vcodec^=vp9][height>=1080][fps>30]/bestvideo[vcodec^=avc1][height>=1080][fps>30]/bestvideo[height>=1080][fps>30]/bestvideo[vcodec^=av01][height>=1080]/bestvideo[vcodec^=vp9.2][height>=1080]/bestvideo[vcodec^=vp9][height>=1080]/bestvideo[vcodec^=avc1][height>=1080]/bestvideo[height>=1080]/bestvideo[vcodec^=av01][height>=720][fps>30]/bestvideo[vcodec^=vp9.2][height>=720][fps>30]/bestvideo[vcodec^=vp9][height>=720][fps>30]/bestvideo[vcodec^=avc1][height>=720][fps>30]/bestvideo[height>=720][fps>30]/bestvideo[vcodec^=av01][height>=720]/bestvideo[vcodec^=vp9.2][height>=720]/bestvideo[vcodec^=vp9][height>=720]/bestvideo[vcodec^=avc1][height>=720]/bestvideo[height>=720]/bestvideo[vcodec^=av01][height>=480][fps>30]/bestvideo[vcodec^=vp9.2][height>=480][fps>30]/bestvideo[vcodec^=vp9][height>=480][fps>30]/bestvideo[vcodec^=avc1][height>=480][fps>30]/bestvideo[height>=480][fps>30]/bestvideo[vcodec^=av01][height>=480]/bestvideo[vcodec^=vp9.2][height>=480]/bestvideo[vcodec^=vp9][height>=480]/bestvideo[vcodec^=avc1][height>=480]/bestvideo[height>=480]/bestvideo[vcodec^=av01][height>=360][fps>30]/bestvideo[vcodec^=vp9.2][height>=360][fps>30]/bestvideo[vcodec^=vp9][height>=360][fps>30]/bestvideo[vcodec^=avc1][height>=360][fps>30]/bestvideo[height>=360][fps>30]/bestvideo[vcodec^=av01][height>=360]/bestvideo[vcodec^=vp9.2][height>=360]/bestvideo[vcodec^=vp9][height>=360]/bestvideo[vcodec^=avc1][height>=360]/bestvideo[height>=360]/bestvideo[vcodec^=av01][height>=240][fps>30]/bestvideo[vcodec^=vp9.2][height>=240][fps>30]/bestvideo[vcodec^=vp9][height>=240][fps>30]/bestvideo[vcodec^=avc1][height>=240][fps>30]/bestvideo[height>=240][fps>30]/bestvideo[vcodec^=av01][height>=240]/bestvideo[vcodec^=vp9.2][height>=240]/bestvideo[vcodec^=vp9][height>=240]/bestvideo[vcodec^=avc1][height>=240]/bestvideo[height>=240]/bestvideo[vcodec^=av01][height>=144][fps>30]/bestvideo[vcodec^=vp9.2][height>=144][fps>30]/bestvideo[vcodec^=vp9][height>=144][fps>30]/bestvideo[vcodec^=avc1][height>=144][fps>30]/bestvideo[height>=144][fps>30]/bestvideo[vcodec^=av01][height>=144]/bestvideo[vcodec^=vp9.2][height>=144]/bestvideo[vcodec^=vp9][height>=144]/bestvideo[vcodec^=avc1][height>=144]/bestvideo[height>=144]/bestvideo)+(bestaudio[acodec^=opus]/bestaudio)/best';
 const defaultFormatCodeAudioOnly = '(bestaudio[acodec^=opus]/bestaudio)/best';
