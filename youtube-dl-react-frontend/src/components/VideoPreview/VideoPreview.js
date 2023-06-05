3178c88178eec362a29560af35259abc52aa019d
Graham Walker <contact@gwalkerux.com>
2023-06-05 03:20:46 -0400

    Fallback for unknown video

--- youtube-dl-react-frontend/src/components/VideoPreview/VideoPreview.js
+++ youtube-dl-react-frontend/src/components/VideoPreview/VideoPreview.js
@@ -16,82 +16,155 @@ const VideoPreview = props => {
     const userContext = useContext(UserContext);
 
     const video = props.video;
-    const uploaderLink = `/uploaders/${video.uploaderDocument?.extractor}/${video.uploaderDocument?.id}`
-    const videoLink = `/videos/${video.extractor}/${video.id}`;
 
-    const videoInfo = <>
-        <div className="media-container">
-            {!!props.icon && video.uploaderDocument &&
-                <Link
-                    to={uploaderLink}
-                    className="me-2"
-                >
+    if (!video) {
+        const videoInfo = <>
+            <div className="media-container">
+                {!!props.icon && video.uploaderDocument &&
                     <Image
                         width={36}
                         height={36}
-                        src={getImage(video, 'avatar')}
-                        onError={(e) => { defaultImage(e, 'avatar') }}
+                        src="/default-avatar.jpg"
                         roundedCircle={userContext.user?.useCircularAvatars ?? true}
                         className="mt-1"
                     />
-                </Link>
-            }
-            <div className="media-body">
-                <Link
-                    className={`text-dark fw-bold video-title-link${!props.simple ? (props.horizontal ? ' mb-1' : ' my-1') : ''}`}
-                    to={videoLink}
-                    title={video.title}
-                >
-                    {video.title}
-                </Link>
-                {video.uploaderDocument &&
-                    <Link
-                        className="video-uploader-link"
-                        to={uploaderLink}
-                    >
-                        <small>{video.uploaderDocument.name}</small>
-                    </Link>
                 }
-                {!!props.simple ||
-                    <Row>
-                        <Col xs="auto">
-                            <small className="text-muted">
-                                {!!video.viewCount &&
-                                    <span title={video.viewCount.toLocaleString() + ' views'}>
-                                        {abbreviateNumber(video.viewCount) + ' views'}
-                                    </span>
-                                }
-                                {!!video.viewCount && video.uploadDate &&
-                                    <span> &middot; </span>
-                                }
-                                {video.uploadDate &&
-                                    <span title={new Date(video.uploadDate).toLocaleDateString()}>
-                                        {dateToTimeSinceString(new Date(video.uploadDate))}
-                                    </span>
-                                }
-                            </small>
-                        </Col>
-                        {!!video.width && !!video.height && resolutionToBadge(video.width, video.height) &&
-                            <Col
-                                xs="12"
-                                md={!!props.horizontal || 'auto'}
-                                className="ms-auto"
-                            >
-                                <small>
-                                    <Badge bg="light">
-                                        {resolutionToBadge(video.width, video.height)}
-                                    </Badge>
-                                </small>
-                            </Col>
-                        }
-                    </Row>
+                <div className="media-body">
+                    <span
+                        className="video-title-link fw-bold text-dark"
+                        title="Unknown Video"
+                        style={{ textDecoration: 'none' }}
+                    >Unknown Video</span>
+                </div>
+            </div>
+        </>;
+
+        const videoImage = <>
+            <div
+                className="video-thumbnail bg-light"
+                style={{ width: props.width }}
+            >
+                <Image
+                    style={{ filter: props.watched ? 'brightness(75%)' : 'none', borderRadius: (userContext.user?.useCircularAvatars ?? true) ? '0.5rem' : 0 }}
+                    src="/default-thumbnail.jpg"
+                    onLoad={(e) => e.target.parentElement.style.setProperty('background-color', 'transparent', 'important')}
+                />
+                {!!props.watched &&
+                    <Badge
+                        bg="dark"
+                        className="video-badge badge-left"
+                    >
+                        <FontAwesomeIcon icon="history" />
+                        <> Watched</>
+                    </Badge>
                 }
             </div>
-        </div>
-    </>;
+        </>;
 
-    const videoImage =
-        <>
+        if (!!props.horizontal) {
+            return (
+                <div
+                    className={'media-container ' + props.className}
+                    style={{ lineHeight: '1em' }}
+                >
+                    {props.children}
+                    <span className="position-relative me-2">
+                        {videoImage}
+                    </span>
+                    <div className="media-body pr-3">
+                        {videoInfo}
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div
+                    className={props.className}
+                    style={{ lineHeight: '1em' }}
+                >
+                    <div className="position-relative mb-1">
+                        {videoImage}
+                    </div>
+                    {videoInfo}
+                </div>
+            );
+        }
+    } else {
+        const uploaderLink = `/uploaders/${video.uploaderDocument?.extractor}/${video.uploaderDocument?.id}`;
+        const videoLink = `/videos/${video.extractor}/${video.id}`;
+
+        const videoInfo = <>
+            <div className="media-container">
+                {!!props.icon && video.uploaderDocument &&
+                    <Link
+                        to={uploaderLink}
+                        className="me-2"
+                    >
+                        <Image
+                            width={36}
+                            height={36}
+                            src={getImage(video, 'avatar')}
+                            onError={(e) => { defaultImage(e, 'avatar') }}
+                            roundedCircle={userContext.user?.useCircularAvatars ?? true}
+                            className="mt-1"
+                        />
+                    </Link>
+                }
+                <div className="media-body">
+                    <Link
+                        className={`text-dark fw-bold video-title-link${!props.simple ? (props.horizontal ? ' mb-1' : ' my-1') : ''}`}
+                        to={videoLink}
+                        title={video.title}
+                    >
+                        {video.title}
+                    </Link>
+                    {video.uploaderDocument &&
+                        <Link
+                            className="video-uploader-link"
+                            to={uploaderLink}
+                        >
+                            <small>{video.uploaderDocument.name}</small>
+                        </Link>
+                    }
+                    {!!props.simple ||
+                        <Row>
+                            <Col xs="auto">
+                                <small className="text-muted">
+                                    {!!video.viewCount &&
+                                        <span title={video.viewCount.toLocaleString() + ' views'}>
+                                            {abbreviateNumber(video.viewCount) + ' views'}
+                                        </span>
+                                    }
+                                    {!!video.viewCount && video.uploadDate &&
+                                        <span> &middot; </span>
+                                    }
+                                    {video.uploadDate &&
+                                        <span title={new Date(video.uploadDate).toLocaleDateString()}>
+                                            {dateToTimeSinceString(new Date(video.uploadDate))}
+                                        </span>
+                                    }
+                                </small>
+                            </Col>
+                            {!!video.width && !!video.height && resolutionToBadge(video.width, video.height) &&
+                                <Col
+                                    xs="12"
+                                    md={!!props.horizontal || 'auto'}
+                                    className="ms-auto"
+                                >
+                                    <small>
+                                        <Badge bg="light">
+                                            {resolutionToBadge(video.width, video.height)}
+                                        </Badge>
+                                    </small>
+                                </Col>
+                            }
+                        </Row>
+                    }
+                </div>
+            </div>
+        </>;
+
+        const videoImage = <>
             <div
                 className="video-thumbnail bg-light"
                 style={{ width: props.width }}
@@ -125,38 +198,39 @@ const VideoPreview = props => {
             }
         </>;
 
-    if (!!props.horizontal) {
-        return (
-            <div
-                className={'media-container ' + props.className}
-                style={{ lineHeight: '1em' }}
-            >
-                {props.children}
-                <Link
-                    to={videoLink}
-                    className="position-relative me-2"
+        if (!!props.horizontal) {
+            return (
+                <div
+                    className={'media-container ' + props.className}
+                    style={{ lineHeight: '1em' }}
                 >
-                    {videoImage}
-                </Link>
-                <div className="media-body pr-3">
+                    {props.children}
+                    <Link
+                        to={videoLink}
+                        className="position-relative me-2"
+                    >
+                        {videoImage}
+                    </Link>
+                    <div className="media-body pr-3">
+                        {videoInfo}
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div
+                    className={props.className}
+                    style={{ lineHeight: '1em' }}
+                >
+                    <Link to={videoLink}>
+                        <div className="position-relative mb-1">
+                            {videoImage}
+                        </div>
+                    </Link>
                     {videoInfo}
                 </div>
-            </div>
-        );
-    } else {
-        return (
-            <div
-                className={props.className}
-                style={{ lineHeight: '1em' }}
-            >
-                <Link to={videoLink}>
-                    <div className="position-relative mb-1">
-                        {videoImage}
-                    </div>
-                </Link>
-                {videoInfo}
-            </div>
-        );
+            );
+        }
     }
 }
 
