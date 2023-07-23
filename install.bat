e94d0f6af4a361b899f3dc14731d921ee8a56663
Graham Walker <contact@gwalkerux.com>
2023-07-23 03:11:42 -0400

    Updated installer scripts

--- install.bat
+++ install.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d "%~dp0"
+call npm install -g pm2
 cd .\youtube-dl-react-frontend
 call npm install
 call npm run build
 cd ..
 cd .\youtube-dl-express-backend
-call npm install -g pm2 
 call npm install
-echo Dependencies installed. Run start-server.bat to launch the web app...
+echo Build complete. Run start-server.bat to launch the web app...
 pause >nul
--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
+sudo apt-get update
+sudo apt-get install xattr
+sudo npm install -g pm2
 cd -P -- "$(dirname -- "$0")"
 cd ./youtube-dl-react-frontend
 npm install --unsafe-perm
 npm run build
 cd ..
 cd ./youtube-dl-express-backend
-apt-get install xattr
-npm install -g pm2
 npm install --unsafe-perm
-echo "Dependencies installed. Run start-server.sh to launch the web app..."
+echo "Build complete. Run start-server.sh to launch the web app..."
