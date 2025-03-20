071f5486d176b72522481c885f8786cdb1d87c2a
Graham Walker <contact@gwalkerux.com>
2025-03-20 15:53:53 -0400

    Import custom player button icons

--- youtube-dl-react-frontend/src/components/Video/ScreenshotButton/ScreenshotButton.js
+++ youtube-dl-react-frontend/src/components/Video/ScreenshotButton/ScreenshotButton.js
@@ -1,4 +1,6 @@
 import videojs from 'video.js';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { Tooltip } from 'bootstrap';
 
 const VjsButton = videojs.getComponent('Button');
@@ -9,8 +11,8 @@ class ScreenshotButton extends VjsButton {
 
         this.controlText('Screenshot');
         
-        this.el().classList.add('vjs-custom-screenshot-button');
-        this.el().innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="camera" class="svg-inline--fa fa-camera" style="font-size: 1.25em;" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"></path></svg>';
+        this.el().classList.add('custom-player-screenshot-button');
+        this.el().innerHTML = icon(faCamera, { classes: ['custom-player-button-icon'] }).html[0];
 
         const tooltipInstance = new Tooltip(this.el(), { trigger: 'manual', title: 'Copied to clipboard!' });
 
--- youtube-dl-react-frontend/src/components/Video/TheaterModeButton/TheaterModeButton.js
+++ youtube-dl-react-frontend/src/components/Video/TheaterModeButton/TheaterModeButton.js
@@ -1,4 +1,6 @@
 import videojs from 'video.js';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faTv } from '@fortawesome/free-solid-svg-icons';
 
 const VjsButton = videojs.getComponent('Button');
 
@@ -8,8 +10,8 @@ class TheaterModeButton extends VjsButton {
 
         this.controlText('Theater Mode');
 
-        this.el().classList.add('vjs-custom-theater-mode-button');
-        this.el().innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tv" class="svg-inline--fa fa-tv " style="font-size: 1.25em;" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M64 64V352H576V64H64zM0 64C0 28.7 28.7 0 64 0H576c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM128 448H512c17.7 0 32 14.3 32 32s-14.3 32-32 32H128c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path></svg>';
+        this.el().classList.add('custom-player-theater-mode-button');
+        this.el().innerHTML = icon(faTv, { classes: ['custom-player-button-icon'] }).html[0];
     }
 
     handleClick() {
